3927028f8d3ccb51446dc154cc5e74ff10d14494
Dianna Tingg <dtingg@gmail.com>
2020-02-20 16:29:40 -0800

    added in order

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,12 @@ class TreeNode
   attr_reader :key, :value
   attr_accessor :left, :right
 
-   def initialize(key, val)
+  def initialize(key, val)
     @key = key
     @value = val
     @left = nil
     @right = nil
-   end
+  end
 end
 
 class Tree
@@ -18,8 +18,45 @@ class Tree
 
   # Time Complexity: 
   # Space Complexity: 
+  # def add(key, value)
+  #   if @root.nil?
+  #     @root = TreeNode.new(key, value)
+  #   else
+  #     current = @root
+
+  #     while true
+  #       if key < current.key
+  #         if !current.left.nil?
+  #           current = current.left
+  #         else
+  #           current.left = TreeNode.new(key, value)
+  #           return
+  #         end
+  #       else
+  #         if !current.right.nil?
+  #           current = current.right
+  #         else
+  #           current.right = TreeNode.new(key, value)
+  #           return
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
+
+  def add_helper(current_node, key, value)
+    return TreeNode.new(key, value) if current_node.nil?
+
+    if key < current_node.key
+      current_node.left = add_helper(current_node.left, key, value)
+    else
+      current_node.right = add_helper(current_node.right, key, value)
+    end
+    return current_node
+  end
+
   def add(key, value)
-    raise NotImplementedError
+    @root = add_helper(@root, key, value)
   end
 
   # Time Complexity: 
@@ -30,8 +67,18 @@ class Tree
 
   # Time Complexity: 
   # Space Complexity: 
+  def inorder_helper(current_node, list)
+    return list if current_node.nil?
+
+    inorder_helper(current_node.left, list)
+    list << { key: current_node.key, value: current_node.value }
+    inorder_helper(current_node.right, list)
+
+    return list
+  end
+
   def inorder
-    raise NotImplementedError
+    return inorder_helper(@root, [])
   end
 
   # Time Complexity: 
