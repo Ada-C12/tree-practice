04c7e83759ad27713aa1177aca959b94aff9a87c
Natalie Tapias <natalie.tapias@nordstrom.com>
2020-02-20 19:58:40 -0800

    Add and Find methods.

--- lib/tree.rb
+++ lib/tree.rb
@@ -19,20 +19,62 @@ class Tree
   # Time Complexity: 
   # Space Complexity: 
   def add(key, value)
-    raise NotImplementedError
+    @root = add_helper(@root, key, value)
+  end
+
+  # recursive helper method 
+  def add_helper(current_node, key, value)
+    #  base case 
+    return TreeNode.new(key, value) if current_node.nil?
+    if key < current_node.key
+      current_node.left = add_helper(current_node.left, key, value)
+    else 
+      current_node.right = add_helper(current_node.right, key, value)
+    end
+    return current_node
+    
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    # raise NotImplementedError
+    current = @root
+    return nil if current.nil?
+    return find_helper(root, key)
+  end
+
+  def find_helper(current_node, key)
+    return current_node.value if current_node.key == key
+
+    if key <= current_node.key
+      current_node.left = find_helper(current_node.left, key)
+    else
+      current_node.right = find_helper(current_node.right, key)
+    end
+
+  end
+
+
+
+  def inorder_helper(current_node, list)
+    return list if current_node.nil? 
+
+    inorder_helper(current_node.left, list)
+    list << { key: current_node.key, value: current_node.value }
+    inorder_helper(current_node.right, list)
+    return list
+      # list << {key: current_node.key, value: current_node.value}
+      # inorder_helper(current_node.left, list)
+    
   end
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return inorder_helper(@root, [])
   end
+
 
   # Time Complexity: 
   # Space Complexity: 
@@ -64,3 +106,30 @@ class Tree
     return "#{self.inorder}"
   end
 end
+
+
+# long form nodes from add 
+ # if @root.nil?
+    #   @root = TreeNode.new(key, value)
+    # else
+      # current = @root
+    
+      # while true
+      # if key < current.key
+      #   if !current.left.nil?
+      #     current = current.left
+      #   else 
+      #     current.left = TreeNode.new(key, value)
+      #     return
+      #   end
+      # else
+      #   if !current.right.nil?
+      #     current = current.right
+      #   else 
+      #     current.right = TreeNode.new(key, value)
+      #     return
+      #   end
+      # end
+
+    
+    # return add_helper(@root, value)
