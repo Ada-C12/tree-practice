46ead49b122d336a2bfe65069788176f6ea075c4
Rinostar <codingrinostar@gmail.com>
2020-02-20 16:30:37 -0800

    1. Add & Find

--- lib/tree.rb
+++ lib/tree.rb
@@ -18,14 +18,60 @@ class Tree
 
   # Time Complexity: 
   # Space Complexity: 
+  def add_helper(curr, key, value)
+    return TreeNode.new(key, value) if curr.nil?
+
+    if key < curr.key
+      curr.left = add_helper(curr.left, key, value)
+    else
+      curr.right = add_helper(curr.right, key, value)
+    end
+
+    return curr
+  end
+
   def add(key, value)
-    raise NotImplementedError
+    @root = TreeNode.new(key, value) if @root.nil?
+
+    add_helper(@root, key, value)
+    # ITERATION SOLUTION: 
+    # curr = @root
+    # while true
+    #   if key < curr.key
+    #     if !curr.left.nil?
+    #       curr = curr.left
+    #     else
+    #       curr.left = TreeNode.new(key, value)
+    #     end
+    #   elsif key > curr.key 
+    #     if !curr.right.nil?
+    #       curr = curr.left
+    #     else
+    #       curr.left = TreeNode.new(key, value)
+    #     end
+    #   else
+    #     curr.value = value
+    #   end
+    # end
   end
 
   # Time Complexity: 
-  # Space Complexity: 
+  # Space Complexity:
   def find(key)
-    raise NotImplementedError
+    return nil if @root.nil?
+
+    curr = @root
+    until curr.nil?
+      if key > curr.key
+        curr = curr.right
+      elsif key < curr.key
+        curr = curr.left
+      else
+        return curr.value
+      end
+    end
+
+    return nil
   end
 
   # Time Complexity: 
