162600dd56eee3a67fce361a53e3ead0060d9fd1
criacuervos <vanessareigosa@gmail.com>
2020-02-23 13:46:20 -0800

    Tests passing for add & find methods (using recursive solution) and inorder traversal method

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,23 +16,57 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def add(key, value)
-    raise NotImplementedError
+    @root = add_helper(@root, key, value)
   end
+
+  def add_helper(current_node, key, value)
+    return TreeNode.new(key, value) if current_node.nil?
+
+    if key < current_node.key
+      current_node.left = add_helper(current_node.left, key, value)
+    else
+      current_node.right = add_helper(current_node.right, key, value)
+    end
+    return current_node
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
   def find(key)
-    raise NotImplementedError
+    return nil if @root == nil? 
+    return find_helper(@root, key)
   end
+
+  def find_helper(current_node, key)
+    return nil if current_node.nil?
+
+    return current_node.value if current_node.key == key 
+
+    if key < current_node.key 
+      find_helper(current_node.left, key)
+    else 
+      find_helper(current_node.right, key)
+    end 
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
   def inorder
-    raise NotImplementedError
+    return inorder_helper(@root, [])
   end
+
+  def inorder_helper(current_node, list)
+    # left, root, right
+    return list if current_node.nil?
+    # recursion; left side first
+    inorder_helper(current_node.left, list)
+    list << {key: current_node.key, value: current_node.value}
+    inorder_helper(current_node.right, list)
+    return list
+  end 
 
   # Time Complexity: 
   # Space Complexity: 
