31da4914813f608768a8d902a54563a85fe75474
Julia Bouvier <juliabouv@gmail.com>
2020-02-25 20:21:40 -0800

    Implemented preorder, postorder and height methods

--- lib/tree.rb
+++ lib/tree.rb
@@ -16,8 +16,8 @@ class Tree
     @root = nil
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: log(n)
+  # Space Complexity: log(n)
   def add(key, value)
     new_node = TreeNode.new(key, value)
 
@@ -40,8 +40,8 @@ class Tree
   end
 
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: log(n)
+  # Space Complexity: log(n)
   def find(key)
     if @root.nil?
       return nil
@@ -61,8 +61,38 @@ class Tree
     end
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  def delete(key)
+    node = find_node(key)
+    unless node.nil?
+      remove(node)
+    end
+  end
+
+  def remove(node)
+    if node.left.nil? && node.right.nil?
+      node = nil
+    elsif !node.left.nil? && node.right.nil?
+      node = node.left
+    elsif node.left.nil? && !node.right.nil?
+      node = node.right
+    else
+      min_node = find_min_node(node.right)
+      node.value = min_node.value
+      min_node = nil
+    end
+  end
+
+  def find_min_node(node)
+    if node.left.nil?
+      min_node = node
+      return min_node
+    else
+      find_min_node(node.left)
+    end
+  end
+
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def inorder
     return inorder_helper(@root, [])
   end
@@ -77,22 +107,51 @@ class Tree
     return list
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def preorder
-    raise NotImplementedError
+    return preorder_helper(@root, [])
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  def preorder_helper(node, list)
+    return list if node.nil?
+
+    list << { key: node.key, value: node.value}
+    preorder_helper(node.left, list)
+    preorder_helper(node.right, list)
+  end
+
+  # Time Complexity: O(n)
+  # Space Complexity: O(n)
   def postorder
-    raise NotImplementedError
+    return postorder_helper(@root, [])
   end
 
-  # Time Complexity: 
-  # Space Complexity: 
+  def postorder_helper(node, list)
+    return list if node.nil?
+
+    postorder_helper(node.left, list)
+    postorder_helper(node.right, list)
+    list << { key: node.key, value: node.value}
+  end
+
+  # Time Complexity: O(n)
+  # Space Complexity: O(log n)
   def height
-    raise NotImplementedError
+    return height_helper(@root, 0, 0)
+  end
+
+  def height_helper(node, left, right)
+    if node.nil?
+      if left >= right
+        return left
+      else
+        return right
+      end
+    end
+
+    height_helper(node.left, left + 1, right)
+    height_helper(node.right, left, right + 1)
   end
 
   # Optional Method
